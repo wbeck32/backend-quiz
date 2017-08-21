08d045e38809b300a09641c1700f4d43ef54aeb6
Wendy Beck <webeck@gmail.com>
2017-08-21 00:03:53 -0700

    another test passing

--- tests/e2e/restaurant-e2e.tests.js
+++ tests/e2e/restaurant-e2e.tests.js
@@ -19,73 +19,72 @@ describe('restaurant API', () => {
 
   it('POST two restaurants and retrieve them ', () => {
     return req
-      .post('/')
-      .send(testRestA)
+    .post('/')
+    .send(testRestA)
+    .then(res => {
+      return req.post('/')
+      .send(testRestB)
       .then(res => {
-        return req.post('/')
-          .send(testRestB)
-          .then(res => {
-          // console.log('B: ', res.body);
-        })
+        // console.log('B: ', res.body);
       })
-      .then(() => {
-        return req.get('/restaurants').then(results => {
-          assert.equal(results.body.length, 2);
-          assert.equal(results.body[1].name, 'Next Door to Food Coma');
-        });
-      })
-      .catch();
+    })
+    .then(() => {
+      return req.get('/restaurants').then(results => {
+        assert.equal(results.body.length, 2);
+        assert.equal(results.body[1].name, 'Next Door to Food Coma');
+      });
+    })
+    .catch();
   }), it('GET restaurant by id', () => {
     return req.post('/')
     .send(testRestC)
     .then(res => {
       return req
-        .get('/:id')
-        .send({ id: res.body._id })
-        .then(found => {
-          assert.equal(found.body.name, testRestC.name);
-        });
-    });
-  }), it('GET restaurant by cuisine', () => {
-    return req
-      .get('/restaurants')
-      .query({ cuisine: 'comfort' })
-      .then(results => {
-        console.log(results.body)
-        assert.equal(results.body.length, 1);
+      .get('/:id')
+      .send({ id: res.body._id })
+      .then(found => {
+        assert.equal(found.body.name, testRestC.name);
       });
-  }), it('POST three reviews from three different users', () => {
+    });
+  }),
+  it('GET restaurant by cuisine', () => {
+    return req
+    .get('/restaurants')
+    .query({ cuisine: 'comfort' })
+    .then(results => {
+      assert.equal(results.body[0].cuisine, 'comfort');
+      assert.equal(results.body.length, 1);
+    });
+  }),
+  it('POST three reviews from three different users', () => {
     return req.get('/restaurants')
-    .then(results =>{
-      console.log(results.body);
-
-    return req.post('/:id/reviews')
-    .send({ review: testHelper.reviews[0], restId: results.body[0]._id })
-    .then(saved => {
-        console.log('in save array: ', saved);
-        return saved;
+    .then(results => {
+      return results.body;
+    })
+    .then(restaurants => {
+      return req.post('/:id/reviews')
+      .send({ review: testHelper.reviews[0], restId: restaurants[0]._id })
+      .then(saved1 => {
+        return req.post('/:id/reviews')
+        .send({ review: testHelper.reviews[1], restId: restaurants[1]._id })
+      })
+      .then(saved2 =>{
+        return req.post('/:id/reviews')
+        .send({ review: testHelper.reviews[2], restId: restaurants[2]._id })
+      })
+      .then(saved3 => {
+        return saved3
+      })
+    })
+    .then(() =>{
+      console.log('all saved');
     })
   })
-  });
-});
+})
 
-//    assert.equal(saved.name, 'Food Coma');
-//     return req.post('/reviews')
-//         .send({ restaurantId: saved._id, reviews: testHelper.reviews })
-//         .then(results => {
-//             // console.log(results);
-//             // assert.equal(results.body[0].cuisine, 'comfort');
-//             return req.post('/reviews')
-//                 .send({ restaurantId: saved._id, reviews: testHelper.dupeReview })
-//                 .then(results => {
-//                     // console.log(results);
-//                     // assert.equal(results.body[0].cuisine, 'comfort');
-//                 })
-//         })
 
-//   * POST two restaurants, each of a different type of cuisine
-//   * **Test** that `GET` `/restaurants` returns both restaurants
-//   * **Test** that `GET` `/restaurants?cuisine=<one of the cuisines>` only returns one of the restaurants
-//   * POST three reviews from three different user emails to one of the restaurants
+
+
+
 //   * POST a fourth review from one of the same emails, **Test** that 400 returned.
 //   * **Test** that `GET` `/restaurants/:id` for that restaurant has the three reviews
